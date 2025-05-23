bada337eb8867779ad14dbc4986471dd7bb2b525
argirowski <argirovski.g@gmail.com>
2025-05-23 10:57:00 +0200

    Adding some fixes

--- reactivities-ui/src/app/shared/components/MapComponent.tsx
+++ reactivities-ui/src/app/shared/components/MapComponent.tsx
@@ -1,6 +1,8 @@
 import { Fragment } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { Icon } from "leaflet";
+import markerIconPng from "leaflet/dist/images/marker-icon.png";
 
 type MapComponentProps = {
   position: [number, number];
@@ -17,7 +19,7 @@ const MapComponent = ({ position, venue }: MapComponentProps) => {
         style={{ height: "100%", width: "100%" }}
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={position}>
+        <Marker position={position} icon={new Icon({ iconUrl: markerIconPng })}>
           <Popup>{venue}</Popup>
         </Marker>
       </MapContainer>
